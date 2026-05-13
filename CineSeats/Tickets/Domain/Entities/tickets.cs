13b89891952ea2299d047fd70fc271ef3b844663
wdfoxy34 <wdfoxy34@gmail.com>
2026-05-12 21:50:15 -0300

    add tentiva de cria o dominio de tickets

---
+++ CineSeats/Tickets/Domain/Entities/tickets.cs
@@ -0,0 +1,10 @@
+﻿namespace CineSeats.Tickets.Domain.Entities;
+
+public class tickets
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public float Preco { get; set; }
+    //por hora so pensei nisso precisamos puxa as infrmações da sala e do flme pra ca 
+    
+}
