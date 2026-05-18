f43bef891fb58c9566bdd55940e8621da4e748c3
wdfoxy34 <wdfoxy34@gmail.com>
2026-05-18 14:29:31 -0300

    desfiz os views para no momento nao termos dor de cabeça

--- CineSeats/Movie Theaters/Domain/Entities/Room.cs
+++ CineSeats/Movie Theaters/Domain/Entities/Room.cs
@@ -5,6 +5,34 @@ public class Room
     public Guid Id { get; set; }
     public int Number { get; set; }
     public List<int> Seats { get; set; }
+    
+    public Room(int number, int seats)
+    {
+        try
+        {
+            if (seats < 10)
+            {
+                throw new ArgumentException("The number of seats cannot be less than 10");
+            }
+        
+            if (number <= 0)
+            {
+                throw new ArgumentNullException("Room number cannot be zero or negative");
+            }
+            
+            Id = Guid.NewGuid();
+            this.Number = number;
+            this.Seats = new List<int>();
+            for (int i = 1; i < seats; i++)
+            {
+                Seats.Add(i);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+    }
 
     
 }
